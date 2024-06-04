37de4268673b9b8a2ff4d9ce2a5bc290ee8b6b56
Mohamad Kholid Bughowi <kholidbughowi@gmail.com>
2024-06-04 20:13:49 +0700

    feat: enable smartindent

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,5 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.cursorlineopt = "number"
-vim.opt.smartindent = false
+-- vim.opt.smartindent = true
 vim.o.background = "dark"
+vim.o.timeoutlen = 10000 -- Timoutlen to delay which key
+vim.g.autoformat = false
