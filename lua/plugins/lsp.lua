335a7ae2d0cb5215c2ab30bafe66b358f89082bc
Mohamad Kholid Bughowi <kholidbughowi@gmail.com>
2024-06-08 10:19:24 +0700

    feat: add sql and git extras

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -10,20 +10,6 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = {
-      setup = {
-        clangd = function(_, opts)
-          opts.capabilities.offsetEncoding = { "utf-16" }
-        end,
-        -- eslint = function()
-        --   require("lazyvim.util").lsp.on_attach(function(client)
-        --     if client.name == "eslint" then
-        --       client.server_capabilities.documentFormattingProvider = true
-        --     elseif client.name == "tsserver" then
-        --       client.server_capabilities.documentFormattingProvider = false
-        --     end
-        --   end)
-        -- end,
-      },
       inlay_hints = { enabled = false },
       document_highlight = {
         enabled = false,
