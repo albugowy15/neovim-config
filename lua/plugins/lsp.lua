1faa3cd4a6731bbf5fc279d56112032d70e05f21
Mohamad Kholid Bughowi <kholidbughowi@gmail.com>
2024-06-10 18:10:09 +0700

    feat: install prettier from extras

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,13 +1,5 @@
 return {
   {
-    "williamboman/mason.nvim",
-    opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, {
-        "prettier",
-      })
-    end,
-  },
-  {
     "neovim/nvim-lspconfig",
     opts = {
       inlay_hints = { enabled = false },
