3aaaf7c89bf4cf8ca55e42e9645b68dbe9785f39
Mohamad Kholid Bughowi <kholidbughowi@gmail.com>
2024-06-16 23:21:19 +0700

    feat: add treesitter support for mdx

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -21,7 +21,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.git" },
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.json" },
-    { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.lang.sql" },
     { import = "lazyvim.plugins.extras.lang.svelte" },
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,9 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = function(_, opts)
-    opts.ensure_installed = opts.ensure_installed or {}
-    vim.list_extend(opts.ensure_installed, { "scss", "css" })
+    if type(opts.ensure_installed) == "table" then
+      vim.list_extend(opts.ensure_installed, { "markdown", "scss", "css" })
+      vim.treesitter.language.register("markdown", "mdx")
+    end
   end,
 }
