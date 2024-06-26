6c523ce8cf3b0b733289ba99229b67338f0a4424
Mohamad Kholid Bughowi <kholidbughowi@gmail.com>
2024-06-26 13:55:11 +0700

    feat: remove flutter

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,13 +1,13 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.o.termguicolors = true
 vim.filetype.add({
   extension = {
     mdx = "mdx",
   },
 })
 vim.opt.cursorlineopt = "number"
--- vim.opt.smartindent = true
 vim.o.background = "dark"
 vim.o.timeoutlen = 5000 -- Timoutlen to delay which key
 vim.g.autoformat = false
